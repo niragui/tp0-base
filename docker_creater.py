f9fe3f1a3066167ed7f6c63a7f9222c1756c8818
niragui <71950097+niragui@users.noreply.github.com>
2023-03-27 02:07:14 -0300

    Arreglo Para Agregar Volumes en la creacion

--- docker_creater.py
+++ docker_creater.py
@@ -37,8 +37,8 @@ def add_clients(amount, file):
         file.write("      - testing_net\n")
         file.write("    depends_on:\n")
         file.write("      - server\n")
-        file.write("    volumes:")
-        file.write("      - ./client/config.yaml:/config.yaml")
+        file.write("    volumes:\n")
+        file.write("      - ./client/config.yaml:/config.yaml\n")
         file.write("\n"*2)
 
 
