c8acd132aebab19fef4ab491a40e56648d6c150f
niragui <ignacioiragu2@gmail.com>
2023-03-29 05:28:00 -0300

    Arreglo Comunicacion Sockets

--- client_bets/common/lotterylocal.py
+++ client_bets/common/lotterylocal.py
@@ -13,18 +13,20 @@ class LotteryLocal():
         self.open = True
 
     def send_clients(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.server, self.port))
         for client in self.clients:
-            if not self.open:
-                break
-            bet = client.get_bet()
-            bet_report = parse_bet(bet, self.address)
-            self.socket.sendall(bet_report.encode())
-            self.socket.send(bet_report)
-            dni = bet.get("DNI")
-            apuesta = bet.get("Apuesta")
-            error = get_reply(self.socket)
+            try:
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket.connect((self.server, self.port))
+                if not self.open:
+                    break
+                bet = client.get_bet()
+                bet_report = parse_bet(bet, self.address)
+                self.socket.sendall(bet_report.encode())
+                dni = bet.get("DNI")
+                apuesta = bet.get("Apuesta")
+                error = get_reply(self.socket)
+            except Exception as err:
+                error = str(err)
 
             if error is None:
                 log_text = "action: apuesta_enviada | result: success"
@@ -34,6 +36,7 @@ class LotteryLocal():
                 log_text += f" | error: {error}"
 
             logging.info(log_text)
+            self.socket.close()
 
     def close_store(self):
         self.socket.close()
