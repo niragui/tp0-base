63a1e4ea3d715fd1ca1b53237670de739628f69d
niragui <ignacioiragu2@gmail.com>
2023-03-30 04:04:36 -0300

    Casteo Cantidad De Agencies

--- server/servermain.py
+++ server/servermain.py
@@ -27,7 +27,7 @@ def initialize_config():
         config_params["port"] = int(os.getenv('SERVER_PORT', config["DEFAULT"]["SERVER_PORT"]))
         config_params["listen_backlog"] = int(os.getenv('SERVER_LISTEN_BACKLOG', config["DEFAULT"]["SERVER_LISTEN_BACKLOG"]))
         config_params["logging_level"] = os.getenv('LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
-        config_params["agencies"] = os.getenv('AGENCIES', config["DEFAULT"]["AGENCIES"])
+        config_params["agencies"] = int(os.getenv('AGENCIES', config["DEFAULT"]["AGENCIES"]))
     except KeyError as e:
         raise KeyError("Key was not found. Error: {} .Aborting server".format(e))
     except ValueError as e:
