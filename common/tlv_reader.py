fa290130b0a5e9f4fa73c69b71101037b9581407
niragui <ignacioiragu2@gmail.com>
2023-03-31 04:36:48 -0300

    Agrego TLV para winners

---
+++ common/tlv_reader.py
@@ -0,0 +1,23 @@
+from .socket import read_socket, read_int
+import logging
+
+
+class TLVReader():
+    def __init__(self, attr_types):
+        self.types = attr_types
+
+    def read(self, socket):
+        amount = read_int(socket)
+
+        values_read = {}
+
+        for i in range(amount):
+            attr_type = read_socket(socket, 1)
+            length = read_int(socket)
+            value = read_socket(socket, length).decode("latin-1")
+            attribute = self.types.get(attr_type, None)
+            if attribute is None:
+                raise Exception(f"{attr_type} Type Not A Valid Type")
+            values_read.update({attribute: value})
+
+        return values_read
